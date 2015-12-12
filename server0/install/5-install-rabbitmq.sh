d3531d91b41f9e609c06883280ecdca35bb21cf1
root <root@server1.(none)>
2015-12-12 13:24:25 +0800

    重构env 12.12

---
+++ server0/install/5-install-rabbitmq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /web/env/soft/
+tar zxvf rabbitmq-server-generic-unix-3.5.3.tar.gz
+
+mv rabbitmq_server-3.5.3/ /usr/local/
+
+echo "export PATH=\$PATH:/usr/local/rabbitmq_server-3.5.3/sbin" >> ~/.bash_profile
+mkdir /etc/rabbitmq
+ln -s /web/deploy/etc/rabbitmq/rabbitmq.config /etc/rabbitmq/
+
+rabbitmqctl add_vhost /app
+rabbitmqctl add_user shitouren UWvQC724myihbJSpmkoH
+rabbitmqctl delete_user guest
+rabbitmqctl set_user_tags shitouren administrator
+rabbitmqctl set_permissions -p /app shitouren ".*" ".*" ".*"
---
+++ server2/server_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#ssh-keygen -t rsa
+#cp authorized_keys ~/.ssh/
+#mkfs -t ext3 /dev/xvdb
+mkdir -p /data
+mount /dev/xvdb /data
+#supervisord
+#nginx
+#sh /web/deploy/bin/fdfs_start.sh
+#sh /web/deploy/bin/nexus_start.sh
+#sh /web/deploy/bin/seafile_start.sh
+
